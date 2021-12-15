775b83090ac68f0117b4e32ad5b187bdc8925467
TheLegendaryTea <91951734+TheLegendaryTea@users.noreply.github.com>
2021-12-15 16:12:28 +0200

    Update Program.cs

--- ConsoleApp27/Program.cs
+++ ConsoleApp27/Program.cs
@@ -6,10 +6,18 @@ namespace ConsoleApp27
     {
         static void Main(string[] args)
         {
-            string line = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
-            Console.WriteLine(line);
+            //7.2
             //это шутка какая то или я не понял задание 
             //средний уровнь 12 задание 
+            string line = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+            Console.WriteLine(line);
+            //это базовый уровень 12 
+            Console.Write("введите строку: ");
+            string str = Console.ReadLine();
+            int result = 0;
+            for (int i = 0; i < str.Length; ++i)
+                result += ((int)str[i]);
+            Console.WriteLine(result);
         }
     }
 }
