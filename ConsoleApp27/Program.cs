2fde6e9c16eea8e34388f7c8c7025e0e3c66a0b7
PC <PC@DESKTOP-VH0UJ4V>
2021-12-15 16:00:44 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp27/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+            Console.WriteLine(line);
+            //это шутка какая то или я не понял задание 
+            //средний уровнь 12 задание 
+        }
+    }
+}
